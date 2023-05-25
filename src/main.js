97818f971dcd286244f24f6017e6555d520e6693
seon <seonsy44@gmail.com>
2023-05-25 16:09:33 +0900

    render 리팩토링

--- src/main.js
+++ src/main.js
@@ -3,18 +3,20 @@ import { handleTodoSubmit } from "./todolist.js";
 function render() {
   Widget.fragment("fragment")
     .append(Widget.h1("title", { textContent: "TODO LIST" }))
-    .append(Widget.form("todoForm", { onSubmit: handleTodoSubmit }));
-
-  Widget.get("todoForm")
-    .append(Widget.div("inputContaier"))
-    .append(Widget.h3("todoListTitle", { textContent: "TODO" }))
-    .append(Widget.ul("todoList"))
-    .append(Widget.h3("doneListTitle", { textContent: "DONE" }))
-    .append(Widget.ul("doneList"));
-
-  Widget.get("inputContaier")
-    .append(Widget.textInput("todoInput", { name: "todoValue" }))
-    .append(Widget.button("submitBtn", { type: "submit", label: "입력" }));
+    .append(
+      Widget.form("todoForm", { onSubmit: handleTodoSubmit })
+        .append(
+          Widget.div("inputContaier")
+            .append(Widget.textInput("todoInput", { name: "todoValue" }))
+            .append(
+              Widget.button("submitBtn", { type: "submit", label: "입력" })
+            )
+        )
+        .append(Widget.h3("todoListTitle", { textContent: "TODO" }))
+        .append(Widget.ul("todoList"))
+        .append(Widget.h3("doneListTitle", { textContent: "DONE" }))
+        .append(Widget.ul("doneList"))
+    );
 
   document.body.append(Widget.get("fragment").getEl());
 }
